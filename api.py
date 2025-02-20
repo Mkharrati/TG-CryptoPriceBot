b5f2121fc34873029c44c2fc04f161affa4f935a
MKharrati <mohammad86kh51@gmail.com>
2025-02-21 00:46:40 +0330

    feat: add some api functions and inital bot setup and testing

--- api.py
+++ api.py
@@ -1,14 +1,51 @@
 import requests
 
-usdtirt_url = "https://api.nobitex.ir/v2/orderbook/USDTIRT"
+# assign const variables
+USDTIRT_url = "https://api.nobitex.ir/v2/orderbook/USDTIRT"
+currency_api_url = "https://api.coinpaprika.com/v1/tickers"
 
 def USDTIRT_price():
     """
     get USDTIRT price
     """
-    usdtirt_response = requests.get(usdtirt_url)
-    usdtirt_price = usdtirt_response.json()["lastTradePrice"]
-    return usdtirt_price
+    response = requests.get(USDTIRT_url)
+    USDTIRT_price = response.json()["lastTradePrice"]
+    return USDTIRT_price
 
-def get_currency_by_symbol():
+def get_currency_list():
+    """
+    request to currency_api_url
+    """
+    response = requests.get(currency_api_url)
+    return response.json()
+
+def list_of_available_currencies():
+    """
+    Return a dictionary of all available currencies (key: symbol of corresponding currency, value: id of corresponding currency)
+    """
+    currency_list = get_currency_list()
+    currency_dict = {}
+    for currency in currency_list:
+        currency_symbol = currency["symbol"]
+        currency_id = currency["id"]
+        currency_dict[currency_symbol] = currency_id
+    return currency_dict
+
+def request_to(url):
+    """
+    do request and Return response
+    """
+    return requests.get(url)
+
+def get_currency_price_by_symbol(symbol = str()):
+    """
+    Return currency price by symbol
+    """
+    symbol = symbol.upper()
+    currency_id = list_of_available_currencies()[symbol]
+    response = request_to(f"https://api.coinpaprika.com/v1/tickers/{currency_id}")
+    response = response.json()
+    price = response["quotes"]["USD"]["price"]
+    # price = int(price)
+    return price
     pass
--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
+from asyncio import Handle
 import telebot
 import os
+import api
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,3 +10,19 @@ bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
 
 if not bot_token:
     raise ValueError("TELEGRAM_BOT_TOKEN is missing. Please check your .env file.")
+
+bot = telebot.TeleBot(bot_token)
+
+@bot.message_handler(commands=["start"])
+def handle_start(message):
+    bot.send_message(message.chat.id, "Hello! \n Send Me desired currency symbol to see price :")
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    text = message.text.upper()
+    if text in api.list_of_available_currencies():
+        currency_id = message.text
+        currency_price = api.get_currency_price_by_symbol(currency_id)
+        bot.send_message(message.chat.id, currency_price)
+
+bot.polling()
